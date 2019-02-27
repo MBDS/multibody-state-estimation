d5040e1208967b02fbbb28abe82c9fe3481cd0ce
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2019-02-27 19:11:40 +0100

    start using gtsam

--- libsparsembs/src/MRF_optimizer.cpp
+++ libsparsembs/src/MRF_optimizer.cpp
@@ -1,9 +1,25 @@
 #include <sparsembs/MRF_optimizer.h>
 
+#if defined(SPARSEMBS_HAVE_GTSAM)
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#endif
+
 using namespace sparsembs;
 using namespace Eigen;
 
 void sparsembs::MRF_optimizer(
 	const MRF_Optimize_Inputs& in, MRF_Optimize_Results& results)
 {
+#if defined(SPARSEMBS_HAVE_GTSAM)
+	MRPT_TRY_START
+
+	gtsam::NonlinearFactorGraph graph;
+
+	gtsam::LevenbergMarquardtOptimizer solver;
+
+	MRPT_TRY_END
+#else
+	throw std::runtime_error("Requires GTSAM");
+#endif
 }
