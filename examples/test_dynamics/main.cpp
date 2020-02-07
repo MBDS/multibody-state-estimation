5a38e84349763bf20a9ff619cc9f2b7e01798db5
Antonio Leanza <Antonio891@users.noreply.github.com>
2020-02-07 13:43:29 +0100

    initial values and priors inserted into test_dynamics

--- examples/test_dynamics/main.cpp
+++ examples/test_dynamics/main.cpp
@@ -66,7 +66,7 @@ void test_dynamics()
 	using gtsam::symbol_shorthand::Q;
 	using gtsam::symbol_shorthand::V;
 	using gtsam::symbol_shorthand::A;
-	using namespace sparsembs;
+	//using namespace sparsembs; /Already used in the global section above
 
 	// Create the multibody object:
 	CModelDefinition model;
@@ -77,7 +77,7 @@ void test_dynamics()
 
 	CDynamicSimulator_R_matrix_dense dynSimul(aMBS);
 
-	// Create the factor graph:
+	// Create the empty factor graph:
 	gtsam::NonlinearFactorGraph graph;
 	gtsam::Values initValues;
 
@@ -104,39 +104,30 @@ void test_dynamics()
 	graph.emplace_shared<FactorTrapInt>(dt, noise_vel, Q(1), Q(2), V(1), V(2));
 	graph.emplace_shared<FactorTrapInt>(dt, noise_acc, V(1), V(2), A(1), A(2));
 
-	// Dynamics:
+	// Create Dynamics factors:
 	graph.emplace_shared<FactorDynamic>(&dynSimul, noise_dyn, Q(0), V(0), A(0));
 	graph.emplace_shared<FactorDynamic>(&dynSimul, noise_dyn, Q(1), V(1), A(1));
 
-	// Continue here!
-
 	// Create null vector:
-	const state_t zeros = gtsam::Vector(gtsam::Vector2(0.0, 0.0));
+	const state_t zeros = gtsam::Vector(gtsam::Vector3(0.0, 0.0));
 
 	// Create Prior vectors:
-	const state_t prior_x1 = gtsam::Vector(gtsam::Vector2(0.0, 0.0));
+	const state_t prior_q1 = gtsam::Vector(gtsam::Vector2(0.0, 0.0));
 	const state_t prior_v1 = gtsam::Vector(gtsam::Vector2(1.0, 2.0));
-	const state_t prior_v2 = gtsam::Vector(gtsam::Vector2(2.0, 3.0));
-	const state_t prior_v3 = gtsam::Vector(gtsam::Vector2(2.0, 3.0));
 
 	// Create Prior factors:
 	graph.emplace_shared<gtsam::PriorFactor<state_t>>(
-		X(1), prior_x1, noise_prior);
+		Q(0), prior_q1, noise_prior);
 	graph.emplace_shared<gtsam::PriorFactor<state_t>>(
-		V(1), prior_v1, noise_prior);
-	graph.emplace_shared<gtsam::PriorFactor<state_t>>(
-		V(2), prior_v2, noise_prior);
-	graph.emplace_shared<gtsam::PriorFactor<state_t>>(
-		V(3), prior_v3, noise_prior);
-
+		V(0), prior_v1, noise_prior);
 
 	// Create initial estimates:
-	initValues.insert(X(1), zeros);
-	initValues.insert(X(2), zeros);
-	initValues.insert(X(3), zeros);
+	initValues.insert(Q(0), zeros);
+	initValues.insert(Q(1), zeros);
+	initValues.insert(V(0), zeros);
 	initValues.insert(V(1), zeros);
-	initValues.insert(V(2), zeros);
-	initValues.insert(V(3), zeros);
+	initValues.insert(A(0), zeros);
+	initValues.insert(A(1), zeros);
 
 	// Run optimizer:
 	graph.print("Factor graph: ");
