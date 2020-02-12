496b11c7c7430946c8f4f41293787e8dffa4eddc
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-02-12 17:14:53 +0100

    save results to file

--- examples/test_dynamics/main.cpp
+++ examples/test_dynamics/main.cpp
@@ -21,160 +21,180 @@ using namespace sparsembs;
 
 void test_dynamics()
 {
-    using gtsam::symbol_shorthand::A;
-    using gtsam::symbol_shorthand::Q;
-    using gtsam::symbol_shorthand::V;
-    // using namespace sparsembs; /Already used in the global section above
+	using gtsam::symbol_shorthand::A;
+	using gtsam::symbol_shorthand::Q;
+	using gtsam::symbol_shorthand::V;
+	// using namespace sparsembs; /Already used in the global section above
 
-    // Create the multibody object:
-    CModelDefinition model;
-    sparsembs::buildFourBarsMBS(model);
+	// Create the multibody object:
+	CModelDefinition model;
+	sparsembs::buildFourBarsMBS(model);
 
-    std::shared_ptr<CAssembledRigidModel> aMBS = model.assembleRigidMBS();
-    aMBS->setGravityVector(0, -9.81, 0);
+	std::shared_ptr<CAssembledRigidModel> aMBS = model.assembleRigidMBS();
+	aMBS->setGravityVector(0, -9.81, 0);
 
-    CDynamicSimulator_R_matrix_dense dynSimul(aMBS);
+	CDynamicSimulator_R_matrix_dense dynSimul(aMBS);
 
-    // Must be called before solve_ddotq(), needed inside the dynamics factors
-    dynSimul.prepare();
+	// Must be called before solve_ddotq(), needed inside the dynamics factors
+	dynSimul.prepare();
 
-    // Create the empty factor graph:
-    gtsam::NonlinearFactorGraph graph;
-    gtsam::Values initValues;
+	// Create the empty factor graph:
+	gtsam::NonlinearFactorGraph graph;
+	gtsam::Values initValues;
 
-    // Add factors:
-    // Create factor noises:
-    const auto n = aMBS->m_q.size();
+	// Add factors:
+	// Create factor noises:
+	const auto n = aMBS->m_q.size();
 
-    const double noise_vel_sigma = 0.01, noise_acc_sigma = 0.01;
+	const double noise_vel_sigma = 0.01, noise_acc_sigma = 0.01;
 
-    auto noise_vel = gtsam::noiseModel::Isotropic::Sigma(n, noise_vel_sigma);
-    auto noise_acc = gtsam::noiseModel::Isotropic::Sigma(n, noise_acc_sigma);
+	auto noise_vel = gtsam::noiseModel::Isotropic::Sigma(n, noise_vel_sigma);
+	auto noise_acc = gtsam::noiseModel::Isotropic::Sigma(n, noise_acc_sigma);
 
-    // x1, *y1*, x2, y2
-    // 0   1     2   3
-    std::vector<size_t> indep_coord_indices;
-    indep_coord_indices.push_back(1);
+	// x1, *y1*, x2, y2
+	// 0   1     2   3
+	std::vector<size_t> indep_coord_indices;
+	indep_coord_indices.push_back(1);
 
-    // Velocity prior: large sigma for all dq(i), except dq(i_indep)
-    gtsam::Vector prior_dq_sigmas;
-    const double large_std = 1e6;
-    const double small_std = 1e-3;
-    prior_dq_sigmas.setConstant(n, large_std);
-    for (auto idx : indep_coord_indices) prior_dq_sigmas(idx) = small_std;
+	// Velocity prior: large sigma for all dq(i), except dq(i_indep)
+	gtsam::Vector prior_dq_sigmas;
+	const double large_std = 1e6;
+	const double small_std = 1e-3;
+	prior_dq_sigmas.setConstant(n, large_std);
+	for (auto idx : indep_coord_indices) prior_dq_sigmas(idx) = small_std;
 
-    auto noise_prior_dq = gtsam::noiseModel::Diagonal::Sigmas(prior_dq_sigmas);
-    auto noise_prior_q = gtsam::noiseModel::Isotropic::Sigma(n, 0.1);
-    auto noise_dyn = gtsam::noiseModel::Isotropic::Sigma(n, 0.1);
+	auto noise_prior_dq = gtsam::noiseModel::Diagonal::Sigmas(prior_dq_sigmas);
+	auto noise_prior_q = gtsam::noiseModel::Isotropic::Sigma(n, 0.1);
+	auto noise_dyn = gtsam::noiseModel::Isotropic::Sigma(n, 0.1);
 
-    const double dt = 0.01;
-    const double t_end = 2;
-    double t = 0;
-    unsigned int N = t_end / dt;
+	const double dt = 0.01;
+	const double t_end = 2;
+	double t = 0;
+	unsigned int N = t_end / dt;
 
-    // Create null vector, for use in velocity and accelerations:
-    const state_t zeros = gtsam::Vector(gtsam::Vector::Zero(n, 1));
+	// Create null vector, for use in velocity and accelerations:
+	const state_t zeros = gtsam::Vector(gtsam::Vector::Zero(n, 1));
 
-    // Create a feasible Q(0):
-    aMBS->m_q.setZero();
-    aMBS->m_dotq.setZero();
-    aMBS->m_ddotq.setZero();
+	// Create a feasible Q(0):
+	aMBS->m_q.setZero();
+	aMBS->m_dotq.setZero();
+	aMBS->m_ddotq.setZero();
 
-    CAssembledRigidModel::TComputeDependentParams cdp;  // default params
-    CAssembledRigidModel::TComputeDependentResults cdr;
-    // Solve the position problem:
-    aMBS->m_q[0] = 1;
-    aMBS->m_q[1] = 0.1;
-    aMBS->m_q[3] = 5;
+	CAssembledRigidModel::TComputeDependentParams cdp;  // default params
+	CAssembledRigidModel::TComputeDependentResults cdr;
+	// Solve the position problem:
+	aMBS->m_q[0] = 1;
+	aMBS->m_q[1] = 0.1;
+	aMBS->m_q[3] = 5;
 
-    aMBS->computeDependentPosVelAcc(indep_coord_indices, true, true, cdp, cdr);
-    std::cout << "Position problem final |Phi(q)|=" << cdr.pos_final_phi
-              << "\n";
-    ASSERT_BELOW_(cdr.pos_final_phi, 1e-4);
+	aMBS->computeDependentPosVelAcc(indep_coord_indices, true, true, cdp, cdr);
+	std::cout << "Position problem final |Phi(q)|=" << cdr.pos_final_phi
+			  << "\n";
+	ASSERT_BELOW_(cdr.pos_final_phi, 1e-4);
 
-    // Extract m_q from the assembled multibody problem:
-    state_t q_0 = gtsam::Vector(aMBS->m_q);
-    std::cout << "q0: " << q_0.transpose() << "\n";
+	// Extract m_q from the assembled multibody problem:
+	state_t q_0 = gtsam::Vector(aMBS->m_q);
+	std::cout << "q0: " << q_0.transpose() << "\n";
 
-    // Create Prior factors:
-    graph.emplace_shared<gtsam::PriorFactor<state_t>>(Q(0), q_0, noise_prior_q);
-    graph.emplace_shared<gtsam::PriorFactor<state_t>>(
-        V(0), zeros, noise_prior_dq);
+	// Create Prior factors:
+	graph.emplace_shared<gtsam::PriorFactor<state_t>>(Q(0), q_0, noise_prior_q);
+	graph.emplace_shared<gtsam::PriorFactor<state_t>>(
+		V(0), zeros, noise_prior_dq);
 
-    for (unsigned int nn = 0; nn < N; nn++, t += dt)
-    {
-        // Create Trapezoidal Integrator factors:
-        graph.emplace_shared<FactorTrapInt>(
-            dt, noise_vel, Q(nn), Q(nn + 1), V(nn), V(nn + 1));
-        graph.emplace_shared<FactorTrapInt>(
-            dt, noise_acc, V(nn), V(nn + 1), A(nn), A(nn + 1));
+	for (unsigned int nn = 0; nn < N; nn++, t += dt)
+	{
+		// Create Trapezoidal Integrator factors:
+		graph.emplace_shared<FactorTrapInt>(
+			dt, noise_vel, Q(nn), Q(nn + 1), V(nn), V(nn + 1));
+		graph.emplace_shared<FactorTrapInt>(
+			dt, noise_acc, V(nn), V(nn + 1), A(nn), A(nn + 1));
 
-        // Create Dynamics factors:
-        graph.emplace_shared<FactorDynamics>(
-            &dynSimul, noise_dyn, Q(nn), V(nn), A(nn));
+		// Create Dynamics factors:
+		graph.emplace_shared<FactorDynamics>(
+			&dynSimul, noise_dyn, Q(nn), V(nn), A(nn));
 
-        // Create initial estimates:
-        initValues.insert(Q(nn), q_0);
-        initValues.insert(V(nn), zeros);
-        initValues.insert(A(nn), zeros);
+		// Create initial estimates:
+		initValues.insert(Q(nn), q_0);
+		initValues.insert(V(nn), zeros);
+		initValues.insert(A(nn), zeros);
 
-        if (nn == N - 1)
-        {
-            // Create Dynamics factors:
-            graph.emplace_shared<FactorDynamics>(
-                &dynSimul, noise_dyn, Q(nn + 1), V(nn + 1), A(nn + 1));
-            // Create initial estimates:
-            initValues.insert(Q(nn + 1), q_0);
-            initValues.insert(V(nn + 1), zeros);
-            initValues.insert(A(nn + 1), zeros);
-        }
-    }
+		if (nn == N - 1)
+		{
+			// Create Dynamics factors:
+			graph.emplace_shared<FactorDynamics>(
+				&dynSimul, noise_dyn, Q(nn + 1), V(nn + 1), A(nn + 1));
+			// Create initial estimates:
+			initValues.insert(Q(nn + 1), q_0);
+			initValues.insert(V(nn + 1), zeros);
+			initValues.insert(A(nn + 1), zeros);
+		}
+	}
 
-    // Run optimizer:
-    std::cout.precision(3);
+	// Run optimizer:
+	std::cout.precision(3);
 
-    graph.print("Factor graph: ");
-    // initValues.print("initValues: ");
-    gtsam::LevenbergMarquardtOptimizer optimizer(graph, initValues);
+	// graph.print("Factor graph: ");
+	// initValues.print("initValues: ");
 
-    const auto& optimValues = optimizer.optimize();
+	gtsam::LevenbergMarquardtParams lmp;
+	lmp.maxIterations = 1000;
 
-    // Process results:
-    optimValues.print("optimValues");
+	gtsam::LevenbergMarquardtOptimizer optimizer(graph, initValues, lmp);
 
-    // Save graph:
-    if (0)
-    {
-        std::ofstream f("graph.dot");
-        gtsam::GraphvizFormatting gvf;
-        graph.saveGraph(f, optimValues, gvf);
-    }
+	const auto& optimValues = optimizer.optimize();
 
-    std::cout << "Initial factors error: " << graph.error(initValues) << "\n";
-    std::cout << "Final factors error: " << graph.error(optimValues) << "\n";
-    std::cout << "Optimization iterations: " << optimizer.iterations() << "\n";
+	// Process results:
+	optimValues.print("optimValues");
 
-    /* mrpt::gui::CDisplayWindowPlots win;
+	// Save states to files:
+	mrpt::math::CMatrixDouble Qs(N, n), dotQs(N, n), ddotQs(N, n);
+	for (unsigned int step = 0; step < N; step++)
+	{
+		const state_t q_val = optimValues.at<state_t>(Q(step));
+		const state_t dq_val = optimValues.at<state_t>(V(step));
+		const state_t ddq_val = optimValues.at<state_t>(A(step));
 
-    std::vector<double> xs, ys;
-    for (int i = 0; i < 100; i++)
-    {
-        xs.push_back(i);
-        ys.push_back(sin(i * 0.01));
-    }
+		Qs.row(step) = q_val;
+		dotQs.row(step) = q_val;
+		ddotQs.row(step) = q_val;
+	}
+	Qs.saveToTextFile("q.txt");
+	dotQs.saveToTextFile("dq.txt");
+	ddotQs.saveToTextFile("ddq.txt");
 
-    win.plot(xs, ys, "r.5");
-    win.waitForKey();*/
+	// Save graph:
+	if (0)
+	{
+		std::ofstream f("graph.dot");
+		gtsam::GraphvizFormatting gvf;
+		graph.saveGraph(f, optimValues, gvf);
+	}
+
+	std::cout << "Initial factors error: " << graph.error(initValues) << "\n";
+	std::cout << "Final factors error: " << graph.error(optimValues) << "\n";
+	std::cout << "Optimization iterations: " << optimizer.iterations() << "\n";
+
+	/* mrpt::gui::CDisplayWindowPlots win;
+
+	std::vector<double> xs, ys;
+	for (int i = 0; i < 100; i++)
+	{
+		xs.push_back(i);
+		ys.push_back(sin(i * 0.01));
+	}
+
+	win.plot(xs, ys, "r.5");
+	win.waitForKey();*/
 }
 
 int main()
 {
-    try
-    {
-        test_dynamics();
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Error: " << e.what() << "\n";
-    }
+	try
+	{
+		test_dynamics();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << "\n";
+	}
 }
