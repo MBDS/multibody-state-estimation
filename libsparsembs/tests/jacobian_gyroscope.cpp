3dec3a1dd2961b39ff25fc74a30c4d16ced077ad
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2020-02-29 20:50:11 +0100

    add jacobian tests

--- libsparsembs/tests/jacobian_gyroscope.cpp
+++ libsparsembs/tests/jacobian_gyroscope.cpp
@@ -1,11 +1,130 @@
 #include <gtest/gtest.h>
 
-#include <sparsembs/sparsembs.h>
+#include <sparsembs/model-examples.h>
+#include <sparsembs/dynamic-simulators.h>
+#include <sparsembs/CAssembledRigidModel.h>
+#include <gtsam/inference/Symbol.h>
+#include <sparsembs/FactorGyroscope.h>
+#include <mrpt/math/num_jacobian.h>
 
 using namespace std;
 using namespace sparsembs;
 
+struct NumericJacobParams
+{
+	gtsam::Vector q, dq, ddq;
+
+	/** Especifies which variable are we taking numerical derivatives with
+	 * respect to: 0: q, 1: \dot{q}, 2: \ddot{q}
+	 */
+	int diff_variable = 0;
+	gtsam::NoiseModelFactor2<state_t, state_t>* factor = nullptr;
+};
+
+static void num_err_wrt_state(
+	const gtsam::Vector& new_state, const NumericJacobParams& p,
+	gtsam::Vector& err)
+{
+	auto q = state_t(p.diff_variable == 0 ? new_state : p.q);
+	auto dq = state_t(p.diff_variable == 1 ? new_state : p.dq);
+	auto ddq = state_t(p.diff_variable == 2 ? new_state : p.ddq);
+
+	// Evaluate error:
+	err = p.factor->evaluateError(q, dq);
+}
+
 TEST(Jacobians, gyroscope)
 {
-	//
+	using gtsam::symbol_shorthand::Q;
+	using gtsam::symbol_shorthand::V;
+	using namespace sparsembs;
+
+	// Create the multibody object:
+	CModelDefinition model;
+	sparsembs::buildFourBarsMBS(model);
+
+	std::shared_ptr<CAssembledRigidModel> aMBS = model.assembleRigidMBS();
+	aMBS->setGravityVector(0, -9.81, 0);
+
+	CDynamicSimulator_R_matrix_dense dynSimul(aMBS);
+	// Must be called before solve_ddotq():
+	dynSimul.prepare();
+
+	// Add factors:
+	// Create factor noises:
+	const auto n = aMBS->m_q.size();
+	const auto m = aMBS->m_Phi_q.getNumRows();
+
+	auto noise_gyro = gtsam::noiseModel::Isotropic::Sigma(1, 0.1);
+
+	// The "actual sensor" observation: useless in this particular test:
+	double dummy_sensor_observation = .0;
+
+	// Add a gyro for each bar:
+	FactorGyroscope::shared_ptr factorsGyro[3];
+	for (unsigned int body_idx = 0; body_idx < 3; body_idx++)
+	{
+		factorsGyro[body_idx] = boost::make_shared<FactorGyroscope>(
+			*aMBS, body_idx, dummy_sensor_observation, noise_gyro, Q(1), V(1));
+	}
+
+	// For different instants of time and mechanism positions and velocities,
+	// test the factor jacobian:
+	double t = 0;  // initial time
+	const double t_end = 10.0;  // end simulation time
+	const double t_steps = 1.0;  // "large steps" to run the tests at
+
+	dynSimul.params.time_step = 0.001;  // integrators timesteps
+
+	for (double t = 0; t < t_end;)
+	{
+		const double t_next = t + t_steps;
+		dynSimul.run(t, t_next);
+		t = t_next;
+
+		std::cout << "Evaluating test for t=" << t << "\n";
+		std::cout << "q  =" << aMBS->m_q.transpose() << "\n";
+		std::cout << "dq =" << aMBS->m_dotq.transpose() << "\n";
+
+		// Convert plain Eigen vectors into state_t classes (used as Values in
+		// GTSAM factor graphs):
+		const state_t q = state_t(aMBS->m_q);
+		const state_t dotq = state_t(aMBS->m_dotq);
+
+		for (unsigned int body_idx = 0; body_idx < 3; body_idx++)
+		{
+			// Evaluate theoretical Jacobians:
+			gtsam::Matrix H[2];
+			factorsGyro[body_idx]->evaluateError(q, dotq, H[0], H[1]);
+
+			// Evaluate numerical Jacobians:
+			gtsam::Matrix H_num[2];
+
+			for (int i = 0; i < 2; i++)
+			{
+				NumericJacobParams p;
+				p.q = q.vector();
+				p.dq = dotq.vector();
+				p.diff_variable = i;
+				p.factor = factorsGyro[body_idx].get();
+
+				const gtsam::Vector x = i == 0 ? p.q : p.dq;
+				const gtsam::Vector x_incr =
+					Eigen::VectorXd::Constant(x.rows(), x.cols(), 1e-7);
+
+				mrpt::math::estimateJacobian(
+					x,
+					std::function<void(
+						const gtsam::Vector& new_q, const NumericJacobParams& p,
+						gtsam::Vector& err)>(&num_err_wrt_state),
+					x_incr, p, H_num[i]);
+			}
+
+			std::cout << "Body: " << body_idx << "\n";
+			std::cout << " Theoretical H1= " << H[0] << "\n";
+			std::cout << " Numerical   H1= " << H_num[0] << "\n";
+			std::cout << " Theoretical H2= " << H[1] << "\n";
+			std::cout << " Numerical   H2= " << H_num[1] << "\n";
+		}
+	}
 }
