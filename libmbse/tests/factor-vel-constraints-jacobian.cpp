bfd3bd01cb8ad4e07180a166d9f7c69cb450c4fc
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2021-06-26 02:25:54 +0200

    copyright update

--- libmbse/tests/factor-vel-constraints-jacobian.cpp
+++ libmbse/tests/factor-vel-constraints-jacobian.cpp
@@ -1,8 +1,8 @@
 /*+-------------------------------------------------------------------------+
   |            Multi Body State Estimation (mbse) C++ library               |
   |                                                                         |
-  | Copyright (C) 2014-2020 University of Almeria                           |
-  | Copyright (C) 2020 University of Salento                                |
+  | Copyright (C) 2014-2021 University of Almeria                           |
+  | Copyright (C) 2021 University of Salento                                |
   | See README for list of authors and papers                               |
   | Distributed under 3-clause BSD license                                  |
   |  See: <https://opensource.org/licenses/BSD-3-Clause>                    |
